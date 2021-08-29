759fd37c4f97975b5b811c8d15e5f7b6cb43083e
ALAA ELDIN ALHALLAK <alaaeldinalhallak@gmail.com>
2021-08-29 16:40:02 +0300

    Creating Admin Login System

--- restaurant/routes/web.php
+++ restaurant/routes/web.php
@@ -18,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
 
+Route::get('redirects', [HomeController::class, 'redirects']);
+
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
